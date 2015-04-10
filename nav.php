2f58b4ae8bb28e6773e112e0e3cc0ad7c42b30d9
DindieWestWood <drouet.dimitri@gmail.com>
2015-04-10 10:15:37 +0200

    Ville liste et correction bug menu

---
+++ nav.php
@@ -0,0 +1,11 @@
+<!-- Nav -->
+    <nav id="nav">
+        <ul>
+            <li><a href="index.html">Accueil</a></li>
+            <li><a href="site-tour-main.html">Les sites touristiques</a></li>
+            <li><a href="activite-main.html">Les activités</a></li>
+            <li><a href="events-main.html">Les événements</a></li>
+            <li><a href="carte-main.html">La carte géographique</a></li>
+        </ul>
+    </nav>
+</div>
